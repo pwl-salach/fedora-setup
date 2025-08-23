5a997a1dc94649d12024c193a5ba4cf46945bbc1
pwl-salach <pwl.salach@gmail.com>
2025-08-23 21:53:39 +0200

    fixes for new system

--- configure-theme.sh
+++ configure-theme.sh
@@ -4,20 +4,22 @@ mkdir -p $MAIN_WORKDIR/external
 cd $MAIN_WORKDIR/external
 
 # install dependencies
-sudo dnf install -y gnome-tweaks gnome-themes-extra sassc gtk-murrine-engine
+sudo dnf install -y gnome-tweaks gnome-themes-extra sassc gtk-murrine-engine gnome-shell-extension-user-theme
 
 # color theme
-git clone https://github.com/vinceliuice/Colloid-gtk-theme.gitcd Colloid-gtk-theme
+git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+cd Colloid-gtk-theme
 ./install.sh --libadwaita --theme orange --color dark --tweaks black rimless
 
 cd $MAIN_WORKDIR
-rm $MAIN_WORKDIR/external
+rm -rf $MAIN_WORKDIR/external
 
 # icon theme
 wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
 wget -qO- https://git.io/papirus-folders-install | env PREFIX=$HOME/.local sh
 papirus-folders -C deeporange --theme Papirus-Dark
 
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 # apply settings
 gsettings set org.gnome.desktop.interface gtk-theme 'Colloid-Dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
