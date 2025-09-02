31d19a01411e0045232c1ea70baf234a81031a4b
pwl-salach <pwl.salach@gmail.com>
2025-09-02 16:27:05 +0200

    WezTerm setup in progress + nvim improvements

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -1,37 +1,134 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+local config = wezterm.config_builder()
+local action = wezterm.action
 
-wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window({})
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
-return {
-	font_rules = {
-		{
-			intensity = "Bold",
-			italic = false,
-			font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold" }),
-		},
-		{
-			intensity = "Bold",
-			italic = true,
-			font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold", style = "Italic" }),
-		},
-		{
-			intensity = "Normal",
-			italic = true,
-			font = wezterm.font("JetBrainsMono Nerd Font Mono", { style = "Italic" }),
-		},
-		{
-			intensity = "Normal",
-			italic = false,
-			font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" }),
-		},
-	},
-	enable_tab_bar = false,
-	default_prog = { "/bin/bash", "-c", "${SETUP_REPO_PATH}/tmux-start.sh" },
-	window_decorations = "RESIZE",
-	window_padding = { left = 0, right = 0, top = 0, bottom = 0 }, -- optional: remove padding
-	term = "xterm-256color",
-	color_scheme = "GruvboxDarkHard",
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
 }
+
+local function split_nav(key)
+	return {
+		key = key,
+		mods = "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if pane:Get_users_vars().IS_NVIM == "true" then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = "CTRL" },
+				}, pane)
+			else
+				win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+			end
+		end),
+	}
+end
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+
+config.keys = {
+	{
+		key = "-",
+		mods = "LEADER",
+		action = action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "\\",
+		mods = "LEADER",
+		action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{ key = "x", mods = "LEADER", action = action.CloseCurrentPane({ confirm = true }) },
+	split_nav("h"),
+	split_nav("j"),
+	split_nav("k"),
+	split_nav("l"),
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = action.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "m",
+		mods = "LEADER",
+		action = action.TogglePaneZoomState,
+	},
+	{ key = "[", mods = "LEADER", action = action.ActivateCopyMode },
+	{
+		key = "c",
+		mods = "LEADER",
+		action = action.SpawnTab("CurrentPaneDomain"),
+	},
+
+	{
+		key = "p",
+		mods = "LEADER",
+		action = action.ActivateTabRelative(-1),
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = action.ActivateTabRelative(1),
+	},
+}
+
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = action.ActivateTab(i - 1),
+	})
+end
+
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold" }),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold", style = "Italic" }),
+	},
+	{
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font("JetBrainsMono Nerd Font Mono", { style = "Italic" }),
+	},
+	{
+		intensity = "Normal",
+		italic = false,
+		font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" }),
+	},
+}
+-- config.enable_tab_bar = false
+-- default_prog = { "/bin/bash", "-c", "source ~/.zprofile && ${SETUP_REPO_PATH}/tmux-start.sh" },
+config.window_decorations = "RESIZE"
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 } -- optional: remove padding
+config.font_size = 12
+config.term = "xterm-256color"
+config.color_scheme = "GruvboxDarkHard"
+return config
--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,3 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.python")
+require("gruvbox").setup({
+  contrast = "hard",
+})
+vim.cmd("colorscheme gruvbox")
