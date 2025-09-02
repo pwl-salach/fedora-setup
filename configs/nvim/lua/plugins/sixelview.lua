6322771aca23103d3f0a3e58ccda8ca005a4266c
pwl-salach <pwl.salach@gmail.com>
2025-09-02 11:38:10 +0200

    Update sixelview plugin configuration with specific options

--- configs/nvim/lua/plugins/sixelview.lua
+++ configs/nvim/lua/plugins/sixelview.lua
@@ -1,4 +1,10 @@
 return {
-  "kjuq/sixelview.nvim",
-  opts = {},
+  "pwl-salach/sixelview.nvim",
+  opts = {
+    delay_ms = 500,
+    constraints = {
+      min_width = 200,
+      min_height = 200,
+    },
+  },
 }
